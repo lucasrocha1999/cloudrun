e1b9b6d9f9708935e6803a0d75cd8bddb195e3da
Lucas <lucasrocha446@gmail.com>
2020-09-27 01:34:26 -0300

    port change

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
+  const name = 'World';
   res.send(`Hello ${name}!`);
 });
 
-const port = process.env.PORT || 8080;
+const port = 8080;
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
 });
